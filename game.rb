8a0043ff704c320f9b4cad927bb39c67be838602
Siuming <wsm1992@hotmail.com>
2018-04-25 03:45:48 +0000

    use instance var instead method call

--- game.rb
+++ game.rb
@@ -1,25 +1,24 @@
 class Game
-  attr_accessor :hand1, :hand2, :current, :last_hand, :result, :hand, :round, :layer
+  attr_accessor :hand1, :hand2, :last_hand, :result, :hand, :round, :layer
   @@calculated = YAML.load_file('calculated.yml')
   def initialize(hand1, hand2, last_hand, round=nil, layer=0)
     @hand1 = hand1
     @hand2 = hand2
-    @current = @hand1
     @last_hand = last_hand
     @round = round
     @layer = layer
-    if layer < 2
+    if @layer < 2
       puts 'round:'
-      puts round
+      puts @round
       puts 'layer:'
-      puts layer
+      puts @layer
       puts 'last hand:'
-      puts last_hand
+      puts @last_hand
       puts 'hand1:'
-      puts hand1
+      puts @hand1
       puts ''
       puts 'hand2:'
-      puts hand2
+      puts @hand2
       puts ''
       puts 'calculated:'
       puts @@calculated.length
@@ -33,13 +32,13 @@ class Game
     legals = legal_hands
     legals.each do |hand|
       i = i+1
-      if round.nil?
+      if @round
         flag = "#{i} / #{legal_hands.length}"
       else
-        flag = round
+        flag = @round
       end
-      if hand1.to_a != hand.to_a
-        left_hand = PokerHand.new(hand1.to_a - hand.to_a)
+      if @hand1.to_a != hand.to_a
+        left_hand = PokerHand.new(@hand1.to_a - hand.to_a)
       else
         left_hand = PokerHand.new
       end
@@ -47,7 +46,7 @@ class Game
         @result = true
         @hand = hand
       else
-        g = Game.new(hand2, left_hand, hand, flag, layer + 1)
+        g = Game.new(@hand2, left_hand, hand, flag, @layer + 1)
         if !@@calculated[g.to_s].nil?
           g.result = @@calculated[g.to_s]
         else
@@ -65,7 +64,7 @@ class Game
   end
 
   def legal_hands
-    amount = last_hand.length
+    amount = @last_hand.length
     result = []
     if amount == 1
       result = legal_cards
@@ -85,8 +84,8 @@ class Game
   end
 
   def legal_cards
-    lh = last_hand
-    result = current.to_a.map{|card| PokerHand.new([card])}
+    lh = @last_hand
+    result = @hand1.to_a.map{|card| PokerHand.new([card])}
     if lh == pass
       return result
     else
@@ -97,7 +96,7 @@ class Game
   def legal_pairs
     face = nil
     suit = nil
-    lh = last_hand
+    lh = @last_hand
     if lh == pass
       face = 0
       suit = 0
@@ -106,7 +105,7 @@ class Game
       suit = lh.map{|card| card.suit}.max
     end
     result = []
-    pairs = current.pairs
+    pairs = @hand1.pairs
     pairs.each do |k, v|
       if k > face
         result << PokerHand.new(v)
@@ -121,14 +120,14 @@ class Game
 
   def legal_threes
     face = nil
-    lh = last_hand
+    lh = @last_hand
     if lh == pass
       face = 0
     else
       face = lh.max.face
     end
     result = []
-    threes = current.threes
+    threes = @hand1.threes
     threes.each do |k, v|
       if k > face
         result << PokerHand.new(v[0..2])
@@ -139,9 +138,9 @@ class Game
 
   def legal_fives
     result = []
-    fives = current.fives
+    fives = @hand1.fives
     fives.each do |five|
-      if five > last_hand
+      if five > @last_hand
         result << five
       end
     end
@@ -153,11 +152,11 @@ class Game
   end
 
   def ==(game)
-    hand1 == game.hand1 && hand2 == game.hand2 && last_hand == game.last_hand
+    @hand1 == game.hand1 && @hand2 == game.hand2 && @last_hand == game.last_hand
   end
 
   def to_s
-    "#{hand1.to_s} #{hand2.to_s} #{last_hand.to_s}"
+    "#{@hand1.to_s} #{@hand2.to_s} #{@last_hand.to_s}"
   end
 
   def self.calculated
